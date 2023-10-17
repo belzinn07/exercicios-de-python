7df6c633689c5449c5d0f2d5eeffe091728cdc3f
Belmiro Neto <143403372+belzinn07@users.noreply.github.com>
2023-10-17 10:22:16 -0300

    v3

---
+++ atividade017.py
@@ -0,0 +1,13 @@
+m²=float(input('Digite a área em metros quadrados: {'))
+m_inteiro= m² * 1.0
+litros= 1*6
+latas= 18
+preco_latas= 80
+apenas_latas= litros * latas
+galões= 3,6
+preco_galões= 25
+apenas_galões= litos * galões
+print('Se comprar apenas latas: {} , preco {} '.format(m²/apenas_latas,apenas_latas*preco_latas))
+print(' Se comprar apenas galões: {} , preco {} '.format(m²/apena_galões, apena_galões*preco_galões))
+print('a quantidade de latas é {}, e gaões {}'.format(m_inteiro * latas,m_inteiro * galões))
+print('Preco da mistura de latas e galões'.format((latas* preco_latas)+(galões*preco_galões)))
