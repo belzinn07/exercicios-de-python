630394da0758ee7f329929eaf97c9aa4f31eb74e
Belmiro Neto <143403372+belzinn07@users.noreply.github.com>
2023-10-17 10:48:20 -0300

    v5

--- Atividade 004.py
+++ Atividade 004.py
@@ -1,3 +1,7 @@
-m=float(input('Digite a quantidade de metros: '))
-c= m * 100
-print('Essa quantidade em centrimetros quivale a {} centímetros'. format(c))
+n1=float(input('Digite a nota 1'))
+n2= float(input('Digitea nota 2'))
+n3=float(input('Digite a nota 3'))
+n4= float(input('Digite a nota 4'))
+s=n1+n2+n3+n4
+media=s/4
+print('A média é igual a {}'.format(media))
